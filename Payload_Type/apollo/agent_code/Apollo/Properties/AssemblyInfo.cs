6ad6e20517312857593f6c0b515415993c1a29bf
Dwight Hohnstein <djhohnstein@gmail.com>
2021-08-24 20:29:52 -0700

    changing description of assembly

--- Payload_Type/apollo/agent_code/Apollo/Properties/AssemblyInfo.cs
+++ Payload_Type/apollo/agent_code/Apollo/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("DebugAnalysis")]
-[assembly: AssemblyDescription("Debug Analysis for Microsoft Word")]
+[assembly: AssemblyTitle("Apollo")]
+[assembly: AssemblyDescription("Mythic Agent for Windows")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Microsoft Corporation")]
-[assembly: AssemblyProduct("DebugAnalysis")]
-[assembly: AssemblyCopyright("Copyright ©  2014")]
+[assembly: AssemblyCompany("SpecterOps (@djhohnstein)")]
+[assembly: AssemblyProduct("Apollo")]
+[assembly: AssemblyCopyright("")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
