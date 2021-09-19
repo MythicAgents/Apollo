d10892f164c8dc9510574a3e5ba15fc383cf8326
Dwight Hohnstein <djhohnstein@gmail.com>
2021-09-19 11:34:55 -0700

    maybe?

--- C2_Profiles/smb/mythic/c2_functions/smb.py
+++ C2_Profiles/smb/mythic/c2_functions/smb.py
@@ -3,15 +3,32 @@ from mythic_c2_container.C2ProfileBase import *
 
 class SMB(C2Profile):
     name = "smb"
-    description = "SMB profile to launch a new agent on a named pipe."
+    description = "Communication over SMB named pipes."
     author = "@djhohnstein"
     is_p2p = True
     is_server_routed = True
-    notes = """
-    When choosing this option, this will additionally compile in the SMBClient profile as well
-    """
     parameters = [
-        C2ProfileParameter(name="pipename", description="Pipe Name",randomize=True,format_string="[a-z0-9]{8}\-[a-z0-9]{4}\-[a-z0-9]{4}\-[a-z0-9]{4}\-[a-z0-9]{12}"),
+        C2ProfileParameter(
+            name="pipename",
+            description="Named Pipe",
+            format_string="[a-z0-9]{8}\-[a-z0-9]{4}\-[a-z0-9]{4}\-[a-z0-9]{4}\-[a-z0-9]{12}",
+            randomize=True,
+            required=False,
+        ),
+        C2ProfileParameter(
+            name="killdate",
+            description="Kill Date",
+            parameter_type=ParameterType.Date,
+            default_value=365,
+            required=False,
+        ),
+        C2ProfileParameter(
+            name="encrypted_exchange_check",
+            description="Perform Key Exchange",
+            choices=["T", "F"],
+            required=False,
+            parameter_type=ParameterType.ChooseOne,
+        ),
         C2ProfileParameter(
             name="AESPSK",
             description="Crypto type",
