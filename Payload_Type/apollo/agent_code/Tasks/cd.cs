27d20a330db0679ff225650db07f8ff8b9c83ae0
Dwight Hohnstein <djhohnstein@gmail.com>
2021-10-20 19:20:33 -0700

    first pass at cd/cp

---
+++ Payload_Type/apollo/agent_code/Tasks/cd.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using ApolloInterop.Classes;
+using ApolloInterop.Interfaces;
+using ApolloInterop.Structs.MythicStructs;
+using System.Runtime.Serialization;
+using ApolloInterop.Serializers;
+using System.Threading;
+using System.IO;
+
+namespace Tasks
+{
+    public class cd : Tasking
+    {
+        public cd(IAgent agent, Task task) : base(agent, task)
+        {
+
+        }
+
+        public override void Kill()
+        {
+            _cancellationToken.Cancel();
+        }
+
+        public override System.Threading.Tasks.Task CreateTasking()
+        {
+            return new System.Threading.Tasks.Task(() =>
+            {
+                if (!Directory.Exists(_data.Parameters))
+                {
+                    _agent.GetTaskManager().AddTaskResponseToQueue(CreateTaskResponse(
+                        $"Directory {_data.Parameters} does not exist",
+                        true,
+                        "error"));
+                } else
+                {
+                    Directory.SetCurrentDirectory(_data.Parameters);
+                    _agent.GetTaskManager().AddTaskResponseToQueue(CreateTaskResponse(
+                        $"Working directory set to {Directory.GetCurrentDirectory()}",
+                        true));
+                }
+            }, _cancellationToken.Token);
+        }
+    }
+}
---
+++ Payload_Type/apollo/agent_code/Tasks/cp.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using ApolloInterop.Classes;
+using ApolloInterop.Interfaces;
+using ApolloInterop.Structs.MythicStructs;
+using System.Runtime.Serialization;
+using ApolloInterop.Serializers;
+using System.Threading;
+using System.IO;
+
+namespace Tasks
+{
+    public class cp : Tasking
+    {
+        [DataContract]
+        internal struct CpParameters
+        {
+            [DataMember(Name = "source")]
+            public string SourceFile;
+            [DataMember(Name = "destination")]
+            public string DestinationFile;
+        }
+        public cp(IAgent agent, Task task) : base(agent, task)
+        {
+
+        }
+
+        public override void Kill()
+        {
+            _cancellationToken.Cancel();
+        }
+
+        public override System.Threading.Tasks.Task CreateTasking()
+        {
+            return new System.Threading.Tasks.Task(() =>
+            {
+                CpParameters parameters = _jsonSerializer.Deserialize<CpParameters>(_data.Parameters);
+                TaskResponse resp;
+                try
+                {
+                    File.Copy(parameters.SourceFile, parameters.DestinationFile);
+                    FileInfo source = new FileInfo(parameters.SourceFile);
+                    FileInfo dest = new FileInfo(parameters.DestinationFile);
+                    resp = CreateTaskResponse(
+                        $"Copied {source.FullName} to {source.FullName}",
+                        true,
+                        "completed",
+                        new IMythicMessage[1]
+                        {
+                            new Artifact
+                            {
+                                BaseArtifact = "FileWrite",
+                                ArtifactDetails = $"Wrote {source.Length} to {dest.FullName}"
+                            }
+                        });
+                } catch (Exception ex)
+                {
+                    resp = CreateTaskResponse($"Failed to copy file: {ex.Message}", true, "error");
+                }
+                _agent.GetTaskManager().AddTaskResponseToQueue(resp);
+            }, _cancellationToken.Token);
+        }
+    }
+}
