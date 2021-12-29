c65717e4f4d7ed49f765075cd2b57bf6c6b6f777
Dwight Hohnstein <djhohnstein@gmail.com>
2021-12-29 12:03:24 -0800

    download browser script

--- Payload_Type/apollo/mythic/agent_functions/download.py
+++ Payload_Type/apollo/mythic/agent_functions/download.py
@@ -89,7 +89,7 @@ class DownloadCommand(CommandBase):
     author = "@djhohnstein"
     argument_class = DownloadArguments
     attackmapping = ["T1020", "T1030", "T1041"]
-    browser_script = BrowserScript(script_name="download_new", author="@djhohnstein", for_new_ui=True)
+    browser_script = BrowserScript(script_name="download", author="@djhohnstein", for_new_ui=True)
 
     async def create_tasking(self, task: MythicTask) -> MythicTask:
         if task.args.get_arg("host"):
