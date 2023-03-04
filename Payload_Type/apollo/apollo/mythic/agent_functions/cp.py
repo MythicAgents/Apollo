1d12a16104c39507a6732c0767248620e1337159
its-a-feature <codybthomas@gmail.com>
2023-03-03 20:40:43 -0600

    re-apply two previous PRs for fixing arg parsing

--- Payload_Type/apollo/apollo/mythic/agent_functions/cp.py
+++ Payload_Type/apollo/apollo/mythic/agent_functions/cp.py
@@ -3,27 +3,33 @@ import json
 
 
 class CpArguments(TaskArguments):
-
     def __init__(self, command_line, **kwargs):
         super().__init__(command_line, **kwargs)
         self.args = [
             CommandParameter(
                 name="source",
-                cli_name = "Path",
-                display_name = "Source file to copy.",
+                cli_name="Path",
+                display_name="Source file to copy.",
                 type=ParameterType.String,
-                description='Source file to copy.'),
+                description="Source file to copy.",
+                parameter_group_info=[ParameterGroupInfo(required=True, ui_position=0)],
+            ),
             CommandParameter(
                 name="destination",
                 cli_name="Destination",
                 display_name="Destination path.",
                 type=ParameterType.String,
-                description="Where the new file will be created.")
+                description="Where the new file will be created.",
+                parameter_group_info=[ParameterGroupInfo(required=True, ui_position=1)],
+            ),
         ]
 
     def split_commandline(self):
         if self.command_line[0] == "{":
-            raise Exception("split_commandline expected string, but got JSON object: " + self.command_line)
+            raise Exception(
+                "split_commandline expected string, but got JSON object: "
+                + self.command_line
+            )
         inQuotes = False
         curCommand = ""
         cmds = []
@@ -31,15 +37,15 @@ class CpArguments(TaskArguments):
             c = self.command_line[x]
             if c == '"' or c == "'":
                 inQuotes = not inQuotes
-            if (not inQuotes and c == ' '):
+            if not inQuotes and c == " ":
                 cmds.append(curCommand)
                 curCommand = ""
             else:
                 curCommand += c
-        
+
         if curCommand != "":
             cmds.append(curCommand)
-        
+
         for x in range(len(cmds)):
             if cmds[x][0] == '"' and cmds[x][-1] == '"':
                 cmds[x] = cmds[x][1:-1]
@@ -47,7 +53,6 @@ class CpArguments(TaskArguments):
                 cmds[x] = cmds[x][1:-1]
 
         return cmds
-    
 
     async def parse_arguments(self):
         if self.command_line[0] == "{":
@@ -55,7 +60,11 @@ class CpArguments(TaskArguments):
         else:
             cmds = self.split_commandline()
             if len(cmds) != 2:
-                raise Exception("Invalid number of arguments given. Expected two, but received: {}\n\tUsage: {}".format(cmds, CpCommand.help_cmd))
+                raise Exception(
+                    "Invalid number of arguments given. Expected two, but received: {}\n\tUsage: {}".format(
+                        cmds, CpCommand.help_cmd
+                    )
+                )
             self.add_arg("source", cmds[0])
             self.add_arg("destination", cmds[1])
 
@@ -72,8 +81,8 @@ class CpCommand(CommandBase):
 
     async def create_tasking(self, task: MythicTask) -> MythicTask:
         task.display_params = "-Source {} -Destination {}".format(
-            task.args.get_arg("source"),
-            task.args.get_arg("destination"))
+            task.args.get_arg("source"), task.args.get_arg("destination")
+        )
         return task
 
     async def process_response(self, response: AgentResponse):
