190ef7504daaefd20d58e478a73ba17cf87c48ff
Matt Ehrnschwender <mehrnschwender@specterops.io>
2024-07-30 11:27:45 -0400

    Cleanup execute_pe.py
    Run autoformatters and cleanup trailing whitespace

--- Payload_Type/apollo/apollo/mythic/agent_functions/execute_pe.py
+++ Payload_Type/apollo/apollo/mythic/agent_functions/execute_pe.py
@@ -2,12 +2,9 @@ from distutils.dir_util import copy_tree
 import shutil
 import tempfile
 from mythic_container.MythicCommandBase import *
-import json
 from uuid import uuid4
-from apollo.mythic.sRDI import ShellcodeRDI
 from mythic_container.MythicRPC import *
 from os import path
-import base64
 import os
 import asyncio
 import platform
@@ -15,7 +12,7 @@ import platform
 PRINTSPOOFER_FILE_ID = ""
 MIMIKATZ_FILE_ID = ""
 
-if platform.system() == 'Windows':  
+if platform.system() == "Windows":
     EXECUTE_PE_PATH = "C:\\Mythic\\Apollo\\srv\\ExecutePE.exe"
 else:
     EXECUTE_PE_PATH = "/srv/ExecutePE.exe"
@@ -55,16 +52,21 @@ class ExecutePEArguments(TaskArguments):
                         group_name="Default",
                         ui_position=2
                     ),
-                ]),
+                ],
+            ),
         ]
 
-    async def get_files(self, inputMsg: PTRPCDynamicQueryFunctionMessage) -> PTRPCDynamicQueryFunctionMessageResponse:
+    async def get_files(
+        self, inputMsg: PTRPCDynamicQueryFunctionMessage
+    ) -> PTRPCDynamicQueryFunctionMessageResponse:
         fileResponse = PTRPCDynamicQueryFunctionMessageResponse(Success=False)
-        file_resp = await SendMythicRPCFileSearch(MythicRPCFileSearchMessage(
-            CallbackID=inputMsg.Callback,
-            LimitByCallback=True,
-            Filename="",
-        ))
+        file_resp = await SendMythicRPCFileSearch(
+            MythicRPCFileSearchMessage(
+                CallbackID=inputMsg.Callback,
+                LimitByCallback=True,
+                Filename="",
+            )
+        )
         if file_resp.Success:
             file_names = []
             for f in file_resp.Files:
@@ -79,7 +81,11 @@ class ExecutePEArguments(TaskArguments):
 
     async def parse_arguments(self):
         if len(self.command_line) == 0:
-            raise Exception("Require a PE to execute.\n\tUsage: {}".format(ExecutePECommand.help_cmd))
+            raise Exception(
+                "Require a PE to execute.\n\tUsage: {}".format(
+                    ExecutePECommand.help_cmd
+                )
+            )
         if self.command_line[0] == "{":
             self.load_args_from_json_string(self.command_line)
         else:
@@ -104,18 +110,33 @@ class ExecutePECommand(CommandBase):
         try:
             global EXECUTE_PE_PATH
             agent_build_path = tempfile.TemporaryDirectory()
-            outputPath = "{}/ExecutePE/bin/Release/ExecutePE.exe".format(agent_build_path.name)
+            outputPath = "{}/ExecutePE/bin/Release/ExecutePE.exe".format(
+                agent_build_path.name
+            )
             copy_tree(str(self.agent_code_path), agent_build_path.name)
-            shell_cmd = "dotnet build -c release -p:Platform=x64 {}/ExecutePE/ExecutePE.csproj -o {}/ExecutePE/bin/Release".format(agent_build_path.name, agent_build_path.name)
-            proc = await asyncio.create_subprocess_shell(shell_cmd, stdout=asyncio.subprocess.PIPE, stderr=asyncio.subprocess.PIPE, cwd=agent_build_path.name)
+            shell_cmd = "dotnet build -c release -p:Platform=x64 {}/ExecutePE/ExecutePE.csproj -o {}/ExecutePE/bin/Release".format(
+                agent_build_path.name, agent_build_path.name
+            )
+            proc = await asyncio.create_subprocess_shell(
+                shell_cmd,
+                stdout=asyncio.subprocess.PIPE,
+                stderr=asyncio.subprocess.PIPE,
+                cwd=agent_build_path.name,
+            )
             stdout, stderr = await proc.communicate()
             if not path.exists(outputPath):
-                raise Exception("Failed to build ExecutePE.exe:\n{}".format(stderr.decode() + "\n" + stdout.decode()))
+                raise Exception(
+                    "Failed to build ExecutePE.exe:\n{}".format(
+                        stderr.decode() + "\n" + stdout.decode()
+                    )
+                )
             shutil.copy(outputPath, EXECUTE_PE_PATH)
         except Exception as ex:
             raise Exception(ex)
 
-    async def create_go_tasking(self, taskData: PTTaskMessageAllData) -> PTTaskCreateTaskingMessageResponse:
+    async def create_go_tasking(
+        self, taskData: PTTaskMessageAllData
+    ) -> PTTaskCreateTaskingMessageResponse:
         response = PTTaskCreateTaskingMessageResponse(
             TaskID=taskData.Task.ID,
             Success=True,
@@ -127,55 +148,72 @@ class ExecutePECommand(CommandBase):
 
         taskData.args.add_arg("pipe_name", str(uuid4()))
         mimikatz_path = os.path.abspath(self.agent_code_path / "mimikatz_x64.exe")
-        printspoofer_path = os.path.abspath(self.agent_code_path / "PrintSpoofer_x64.exe")
-        if platform.system() == 'Windows': 
-            shellcode_path =  "C:\\Mythic\\Apollo\\temp\\loader.bin"
+        printspoofer_path = os.path.abspath(
+            self.agent_code_path / "PrintSpoofer_x64.exe"
+        )
+        if platform.system() == "Windows":
+            shellcode_path = "C:\\Mythic\\Apollo\\temp\\loader.bin"
         else:
             shellcode_path = "/tmp/loader.bin"
-        
-        if platform.system() == 'Windows':
+
+        if platform.system() == "Windows":
             donutPath = os.path.abspath(self.agent_code_path / "donut.exe")
         else:
             donutPath = os.path.abspath(self.agent_code_path / "donut")
-        
+
         command = "chmod 777 {}; chmod +x {}".format(donutPath, donutPath)
-        proc = await asyncio.create_subprocess_shell(command, stdout=asyncio.subprocess.PIPE, stderr=asyncio.subprocess.PIPE)
+        proc = await asyncio.create_subprocess_shell(
+            command, stdout=asyncio.subprocess.PIPE, stderr=asyncio.subprocess.PIPE
+        )
         stdout, stderr = await proc.communicate()
 
         if not path.exists(EXECUTE_PE_PATH):
             await self.build_exepe()
 
-        if platform.system() == 'Windows':
-            command = "{} -i {} -p \"{}\"".format(donutPath, EXECUTE_PE_PATH, taskData.args.get_arg("pipe_name"))
+        if platform.system() == "Windows":
+            command = '{} -i {} -p "{}"'.format(
+                donutPath, EXECUTE_PE_PATH, taskData.args.get_arg("pipe_name")
+            )
         else:
-            command = "{} -f 1 {} -p \"{}\"".format(donutPath, EXECUTE_PE_PATH, taskData.args.get_arg("pipe_name"))
-        #print(command)
+            command = '{} -f 1 {} -p "{}"'.format(
+                donutPath, EXECUTE_PE_PATH, taskData.args.get_arg("pipe_name")
+            )
+        # print(command)
         # need to go through one more step to turn our exe into shellcode
-        if platform.system() == 'Windows': 
-            Currentwd =  "C:\\Mythic\\Apollo\\temp\\"
+        if platform.system() == "Windows":
+            Currentwd = "C:\\Mythic\\Apollo\\temp\\"
         else:
             Currentwd = "/tmp"
-        proc = await asyncio.create_subprocess_shell(command, stdout=asyncio.subprocess.PIPE, stderr=asyncio.subprocess.PIPE, cwd=Currentwd)
+        proc = await asyncio.create_subprocess_shell(
+            command,
+            stdout=asyncio.subprocess.PIPE,
+            stderr=asyncio.subprocess.PIPE,
+            cwd=Currentwd,
+        )
         stdout, stderr = await proc.communicate()
 
-        stdout_err = f'[stdout]\n{stdout.decode()}\n'
-        stdout_err = f'[stderr]\n{stderr.decode()}'
+        stdout_err = f"[stdout]\n{stdout.decode()}\n"
+        stdout_err = f"[stderr]\n{stderr.decode()}"
 
         if not path.exists(shellcode_path):
             raise Exception("Failed to create shellcode:\n{}".format(stdout_err))
         else:
             with open(shellcode_path, "rb") as f:
                 shellcode = f.read()
-            file_resp = await SendMythicRPCFileCreate(MythicRPCFileCreateMessage(
-                TaskID=taskData.Task.ID,
-                Filename="execute_pe shellcode",
-                DeleteAfterFetch=False,
-                FileContents=shellcode
-            ))
+            file_resp = await SendMythicRPCFileCreate(
+                MythicRPCFileCreateMessage(
+                    TaskID=taskData.Task.ID,
+                    Filename="execute_pe shellcode",
+                    DeleteAfterFetch=False,
+                    FileContents=shellcode,
+                )
+            )
             if file_resp.Success:
                 taskData.args.add_arg("loader_stub_id", file_resp.AgentFileId)
             else:
-                raise Exception("Failed to register ExecutePE shellcode: " + file_resp.Error)
+                raise Exception(
+                    "Failed to register ExecutePE shellcode: " + file_resp.Error
+                )
 
         # I know I could abstract these routines out but I'm rushing
         if taskData.args.get_arg("pe_name") == "mimikatz.exe":
@@ -184,12 +222,14 @@ class ExecutePECommand(CommandBase):
             else:
                 with open(mimikatz_path, "rb") as f:
                     mimibytes = f.read()
-                file_resp = await SendMythicRPCFileCreate(MythicRPCFileCreateMessage(
-                    TaskID=taskData.Task.ID,
-                    Filename="execute_pe mimikatz",
-                    DeleteAfterFetch=False,
-                    FileContents=mimibytes
-                ))
+                file_resp = await SendMythicRPCFileCreate(
+                    MythicRPCFileCreateMessage(
+                        TaskID=taskData.Task.ID,
+                        Filename="execute_pe mimikatz",
+                        DeleteAfterFetch=False,
+                        FileContents=mimibytes,
+                    )
+                )
                 if file_resp.Success:
                     taskData.args.add_arg(PE_VARNAME, file_resp.AgentFileId)
                     MIMIKATZ_FILE_ID = file_resp.AgentFileId
@@ -201,12 +241,14 @@ class ExecutePECommand(CommandBase):
             else:
                 with open(printspoofer_path, "rb") as f:
                     psbytes = f.read()
-                file_resp = await SendMythicRPCFileCreate(MythicRPCFileCreateMessage(
-                    TaskID=taskData.Task.ID,
-                    Filename="execute_pe printspoofer",
-                    DeleteAfterFetch=False,
-                    FileContents=psbytes
-                ))
+                file_resp = await SendMythicRPCFileCreate(
+                    MythicRPCFileCreateMessage(
+                        TaskID=taskData.Task.ID,
+                        Filename="execute_pe printspoofer",
+                        DeleteAfterFetch=False,
+                        FileContents=psbytes,
+                    )
+                )
                 if file_resp.Success:
                     taskData.args.add_arg(PE_VARNAME, file_resp.AgentFileId)
                     PRINTSPOOFER_FILE_ID = file_resp.AgentFileId
@@ -219,6 +261,8 @@ class ExecutePECommand(CommandBase):
         )
         return response
 
-    async def process_response(self, task: PTTaskMessageAllData, response: any) -> PTTaskProcessResponseMessageResponse:
+    async def process_response(
+        self, task: PTTaskMessageAllData, response: any
+    ) -> PTTaskProcessResponseMessageResponse:
         resp = PTTaskProcessResponseMessageResponse(TaskID=task.Task.ID, Success=True)
         return resp
