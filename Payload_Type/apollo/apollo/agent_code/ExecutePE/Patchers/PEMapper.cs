ddfd192c2cb45785465b136d30ffb38217996f8c
Matt Ehrnschwender <matthewe2020@gmail.com>
2024-08-14 18:41:56 -0400

    Fix relocations
    Handle different relocation types and change relocation logic

--- Payload_Type/apollo/apollo/agent_code/ExecutePE/Patchers/PEMapper.cs
+++ Payload_Type/apollo/apollo/agent_code/ExecutePE/Patchers/PEMapper.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.ComponentModel;
 using System.Runtime.InteropServices;
-using ApolloInterop.Utils;
 using ExecutePE.Helpers;
 using ExecutePE.Internals;
 
@@ -19,9 +18,42 @@ namespace ExecutePE.Patchers
                 NativeDeclarations.MEM_COMMIT, NativeDeclarations.PAGE_READWRITE);
             currentBase = _codebase.ToInt64();
 
+            int relocTableFileOffset = 0;
+            int relocTableFileSize = 0;
+
             // Copy Sections
             for (var i = 0; i < _pe.FileHeader.NumberOfSections; i++)
             {
+                // The relocation table is typically marked as 'SCN_MEM_DISCARDABLE' so it will be discarded.
+                // Check if this section refers to the relocation table and save the file offset.
+                // The relocations are parsed from the file and not the virtual address.
+                if (_pe.OptionalHeader64.BaseRelocationTable.VirtualAddress == _pe.ImageSectionHeaders[i].VirtualAddress)
+                {
+                    try
+                    {
+                        checked
+                        {
+                            relocTableFileOffset = (int)_pe.ImageSectionHeaders[i].PointerToRawData;
+                        }
+                    }
+                    catch (OverflowException)
+                    {
+                        throw new InvalidOperationException("Relocation table file offset is invalid");
+                    }
+
+                    try
+                    {
+                        checked
+                        {
+                            relocTableFileSize = (int)_pe.ImageSectionHeaders[i].SizeOfRawData;
+                        }
+                    }
+                    catch (OverflowException)
+                    {
+                        throw new InvalidOperationException("Relocation table size is invalid");
+                    }
+                }
+
                 // Discard sections marked as discardable
                 if (_pe.ImageSectionHeaders[i].Characteristics.HasFlag(PELoader.SectionFlags.IMAGE_SCN_MEM_DISCARDABLE))
                 {
@@ -50,57 +82,72 @@ namespace ExecutePE.Patchers
                 }
             }
 
-            // Perform Base Relocation
+            // Calculate the delta for relocations
             var delta = currentBase - (long)_pe.OptionalHeader64.ImageBase;
 
-            // Modify Memory Based On Relocation Table
-            var relocationTable =
-                (IntPtr)(currentBase + (int)_pe.OptionalHeader64.BaseRelocationTable.VirtualAddress);
-            var relocationEntry = (NativeDeclarations.IMAGE_BASE_RELOCATION)Marshal.PtrToStructure(relocationTable, typeof(NativeDeclarations.IMAGE_BASE_RELOCATION));
-
-            var imageSizeOfBaseRelocation = Marshal.SizeOf(typeof(NativeDeclarations.IMAGE_BASE_RELOCATION));
-            var nextEntry = relocationTable;
-            var sizeofNextBlock = (int)relocationEntry.SizeOfBlock;
-            var offset = relocationTable;
-
-            while (true)
+            // Get the start of the relocation table from the file offset. Assume that a non-existent
+            // relocation table means that the PE is malformed.
+            if (relocTableFileOffset == 0)
             {
-                var pRelocationTableNextBlock = (IntPtr)(relocationTable.ToInt64() + sizeofNextBlock);
+                throw new InvalidOperationException("Relocation table not found. PE may be malformed.");
+            }
 
-                var relocationNextEntry =
-                    (NativeDeclarations.IMAGE_BASE_RELOCATION)Marshal.PtrToStructure(pRelocationTableNextBlock, typeof(NativeDeclarations.IMAGE_BASE_RELOCATION));
+            var relocationTable = unpacked.AsSpan(relocTableFileOffset, relocTableFileSize);
+            var relocationIndex = 0;
 
-                var pRelocationEntry = (IntPtr)(currentBase + relocationEntry.VirtualAdress);
+            var baseRelocationEntry = NativeDeclarations.IMAGE_BASE_RELOCATION.Parse(relocationTable[relocationIndex..].ToArray());
+            var baseRelocationBlockSize = 8;
 
-                for (var i = 0; i < (int)((relocationEntry.SizeOfBlock - imageSizeOfBaseRelocation) / 2); i++) // TODO figure out magic numbers
+            // Iterate over each entry in the relocation table and apply relocations
+            while (baseRelocationEntry.SizeOfBlock != 0)
+            {
+                IntPtr relocationBaseAddress = (IntPtr)(currentBase + baseRelocationEntry.VirtualAddress);
+
+                var relocationEntriesStart = relocationIndex + baseRelocationBlockSize;
+                var relocationEntriesByteCount = baseRelocationEntry.SizeOfBlock - baseRelocationBlockSize;
+
+                var relocationEntries = relocationTable.Slice(relocationEntriesStart, (int)relocationEntriesByteCount);
+                for (var offset = 0; offset < relocationEntries.Length; offset += 2)
                 {
-                    var value = (ushort)Marshal.ReadInt16(offset, 8 + 2 * i); // TODO figure out magic numbers
-                    var type = (ushort)(value >> 12); // TODO figure out magic numbers
-                    var fixup = (ushort)(value & 0xfff); // TODO figure out magic numbers
+                    var relocEntry = BitConverter.ToUInt16(relocationEntries.ToArray(), offset);
+                    var relocType = (byte)((ushort)(relocEntry & 0xf000) >> 12);
+                    var patchAddress = relocationBaseAddress.Add(relocEntry & 0xfff);
 
-                    switch (type)
+                    switch (relocType)
                     {
-                        case 0x0:
+                        case (byte)NativeDeclarations.X86BaseRelocationType.IMAGE_REL_BASED_ABSOLUTE:
                             break;
-                        case 0xA:
-                            var patchAddress = (IntPtr)(pRelocationEntry.ToInt64() + fixup);
+
+                        case (byte)NativeDeclarations.X86BaseRelocationType.IMAGE_REL_BASED_HIGH:
+                            var addressValue = (uint)Marshal.ReadInt32(patchAddress);
+                            var highValue = ((delta >> 16) + (addressValue >> 16)) << 16;
+                            Marshal.WriteInt32(patchAddress, (int)(highValue | (addressValue & 0xffff)));
+                            break;
+
+                        case (byte)NativeDeclarations.X86BaseRelocationType.IMAGE_REL_BASED_LOW:
+                            addressValue = (uint)Marshal.ReadInt32(patchAddress);
+                            var lowValue = (delta & 0xffff) + (addressValue & 0xffff);
+                            Marshal.WriteInt32(patchAddress, (int)((addressValue & ~0xffff) | lowValue));
+                            break;
+
+                        case (byte)NativeDeclarations.X86BaseRelocationType.IMAGE_REL_BASED_HIGHLOW:
+                            addressValue = (uint)Marshal.ReadInt32(patchAddress);
+                            Marshal.WriteInt32(patchAddress, (int)(addressValue + delta));
+                            break;
+
+                        case (byte)NativeDeclarations.X86BaseRelocationType.IMAGE_REL_BASED_DIR64:
                             var originalAddr = Marshal.ReadInt64(patchAddress);
                             Marshal.WriteInt64(patchAddress, originalAddr + delta);
                             break;
+
+                        default:
+                            throw new InvalidOperationException($"Found an invalid relocation type {relocType}");
                     }
                 }
 
-                offset = (IntPtr)(relocationTable.ToInt64() + sizeofNextBlock);
-                sizeofNextBlock += (int)relocationNextEntry.SizeOfBlock;
-                relocationEntry = relocationNextEntry;
-                nextEntry = (IntPtr)(nextEntry.ToInt64() + sizeofNextBlock);
-
-                if (relocationNextEntry.SizeOfBlock == 0)
-                {
-                    break;
-                }
+                relocationIndex += (int)baseRelocationEntry.SizeOfBlock;
+                baseRelocationEntry = NativeDeclarations.IMAGE_BASE_RELOCATION.Parse(relocationTable[relocationIndex..].ToArray());
             }
-
         }
 
         internal void ClearPE()
@@ -151,7 +198,7 @@ namespace ExecutePE.Patchers
                     protection = NativeDeclarations.PAGE_NOACCESS;
                 }
 
-                var y = NativeDeclarations.VirtualProtect((IntPtr)(_codebase.ToInt64() + _pe.ImageSectionHeaders[i].VirtualAddress),
+                NativeDeclarations.VirtualProtect((IntPtr)(_codebase.ToInt64() + _pe.ImageSectionHeaders[i].VirtualAddress),
                     (UIntPtr)_pe.ImageSectionHeaders[i].SizeOfRawData, protection, out _);
             }
         }
