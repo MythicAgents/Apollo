58716201aef45de1c2e32fa847c28c70e251e4b3
its-a-feature <codybthomas@gmail.com>
2026-02-20 15:31:04 -0600

    adding new parameter for upload

--- Payload_Type/apollo/apollo/mythic/agent_functions/upload.py
+++ Payload_Type/apollo/apollo/mythic/agent_functions/upload.py
@@ -19,6 +19,13 @@ class UploadArguments(TaskArguments):
                 parameter_group_info=[
                     ParameterGroupInfo(
                         required=False,
+                        group_name="Default",
+                        ui_position=2
+                    ),
+                    ParameterGroupInfo(
+                        required=False,
+                        group_name="ExistingFile",
+                        ui_position=2
                     ),
                 ],
             ),
@@ -27,8 +34,51 @@ class UploadArguments(TaskArguments):
                 cli_name="File",
                 display_name="File",
                 type=ParameterType.File,
+                parameter_group_info=[
+                    ParameterGroupInfo(
+                        required=False,
+                        group_name="Default",
+                        ui_position=1
+                    ),
+                ],
+            ),
+            CommandParameter(
+                name="filename",
+                cli_name="fileName",
+                display_name="Existing File Name",
+                type=ParameterType.ChooseOne,
+                dynamic_query_function=self.get_files,
+                description="Name of file that Mythic is already tracking (e.g., Seatbelt.exe).",
+                parameter_group_info=[
+                    ParameterGroupInfo(
+                        required=True, group_name="ExistingFile", ui_position=1
+                    )
+                ],
             ),
         ]
+
+    async def get_files(
+        self, inputMsg: PTRPCDynamicQueryFunctionMessage
+    ) -> PTRPCDynamicQueryFunctionMessageResponse:
+        fileResponse = PTRPCDynamicQueryFunctionMessageResponse(Success=False)
+        file_resp = await SendMythicRPCFileSearch(
+            MythicRPCFileSearchMessage(
+                CallbackID=inputMsg.Callback,
+                LimitByCallback=False,
+                Filename="",
+            )
+        )
+        if file_resp.Success:
+            file_names = []
+            for f in file_resp.Files:
+                if f.Filename not in file_names:
+                    file_names.append(f.Filename)
+            fileResponse.Success = True
+            fileResponse.Choices = file_names
+            return fileResponse
+        else:
+            fileResponse.Error = file_resp.Error
+            return fileResponse
 
     async def parse_dictionary(self, dictionary_arguments):
         self.load_args_from_dictionary(dictionary_arguments)
@@ -85,38 +135,78 @@ class UploadCommand(CommandBase):
             TaskID=taskData.Task.ID,
             Success=True,
         )
-        file_resp = await SendMythicRPCFileSearch(
-            MythicRPCFileSearchMessage(
-                TaskID=taskData.Task.ID, AgentFileID=taskData.args.get_arg("file")
-            )
-        )
-        if file_resp.Success:
-            original_file_name = file_resp.Files[0].Filename
-        else:
-            raise Exception(
-                "Failed to fetch uploaded file from Mythic (ID: {})".format(
-                    taskData.args.get_arg("file")
+        originalGroupName = taskData.args.get_parameter_group_name()
+        if originalGroupName == "Default":
+            file_resp = await SendMythicRPCFileSearch(
+                MythicRPCFileSearchMessage(
+                    TaskID=taskData.Task.ID, AgentFileID=taskData.args.get_arg("file")
                 )
             )
-
-        taskData.args.add_arg(
-            "file_name", original_file_name, type=ParameterType.String
-        )
-
+            if file_resp.Success:
+                original_file_name = file_resp.Files[0].Filename
+            else:
+                raise Exception(
+                    "Failed to fetch uploaded file from Mythic (ID: {})".format(
+                        taskData.args.get_arg("file")
+                    )
+                )
+            taskData.args.add_arg(
+                "file_name", original_file_name, type=ParameterType.String,
+                parameter_group_info=[
+                    ParameterGroupInfo(
+                        required=True, group_name=originalGroupName, ui_position=1
+                    )
+                ],
+            )
+        else:
+            original_file_name = taskData.args.get_arg("filename")
+            file_resp = await SendMythicRPCFileSearch(MythicRPCFileSearchMessage(
+                Filename=taskData.args.get_arg("filename"),
+                TaskID=taskData.Task.ID,
+                MaxResults=1
+            ))
+            if not file_resp.Success:
+                raise Exception(f"failed to find file: {file_resp.Error}")
+            if len(file_resp.Files) == 0:
+                raise Exception(f"no file by that name that's not deleted")
+            taskData.args.remove_arg("file")
+            taskData.args.remove_arg("filename")
+            taskData.args.add_arg("file", file_resp.Files[0].AgentFileId,
+                                  parameter_group_info=[
+                                      ParameterGroupInfo(
+                                          required=True, group_name=originalGroupName, ui_position=1
+                                      )
+                                  ],)
         path = taskData.args.get_arg("remote_path")
         if path is None or path == "":
             path = original_file_name
-            taskData.args.add_arg("remote_path", path)
-            taskData.args.add_arg("host", taskData.Callback.Host)
+            taskData.args.add_arg("remote_path", path, parameter_group_info=[
+                ParameterGroupInfo(
+                    required=True, group_name=originalGroupName, ui_position=1
+                )
+            ])
+            taskData.args.add_arg("host", taskData.Callback.Host, parameter_group_info=[
+                ParameterGroupInfo(
+                    required=True, group_name=originalGroupName, ui_position=1
+                )
+            ])
         if uncmatch := re.match(
             r"^\\\\(?P<host>[^\\]+)\\(?P<path>.*)$",
             path,
         ):
-            taskData.args.add_arg("host", uncmatch.group("host"))
+            taskData.args.add_arg("host", uncmatch.group("host"), parameter_group_info=[
+                ParameterGroupInfo(
+                    required=True, group_name=originalGroupName, ui_position=1
+                )
+            ])
             taskData.args.set_arg("remote_path", uncmatch.group("path"))
         else:
             # Set the host argument to an empty string if it does not exist
-            taskData.args.add_arg("host", taskData.Callback.Host)
+            taskData.args.add_arg("host", taskData.Callback.Host, parameter_group_info=[
+                ParameterGroupInfo(
+                    required=True, group_name=originalGroupName, ui_position=1
+                )
+            ])
         if host := taskData.args.get_arg("host"):
             host = host.upper()
 
@@ -127,13 +217,13 @@ class UploadCommand(CommandBase):
             taskData.args.set_arg("host", host)
         if path is not None and path != "":
             if host is not None and host != "":
-                disp_str = "-File {} -Host {} -Path {}".format(
+                disp_str = "-fileName {} -Host {} -Path {}".format(
                     original_file_name, taskData.args.get_arg("host"), taskData.args.get_arg("remote_path")
                 )
             else:
-                disp_str = "-File {} -Path {}".format(original_file_name, taskData.args.get_arg("remote_path"))
+                disp_str = "-fileName {} -Path {}".format(original_file_name, taskData.args.get_arg("remote_path"))
         else:
-            disp_str = "-File {}".format(original_file_name)
+            disp_str = "-fileName {}".format(original_file_name)
         response.DisplayParams = disp_str
         return response
 
