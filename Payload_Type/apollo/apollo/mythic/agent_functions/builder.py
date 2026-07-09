3b9b9dadfb21b3d336d80fca77a1024f36356a31
Jack Ullrich <jackullrich@users.noreply.github.com>
2026-07-09 15:34:52 -0400

    Load user profile (#188)

--- Payload_Type/apollo/apollo/mythic/agent_functions/builder.py
+++ Payload_Type/apollo/apollo/mythic/agent_functions/builder.py
@@ -140,7 +140,7 @@ class Apollo(PayloadType):
     supported_os = [
         SupportedOS.Windows
     ]
-    semver = "2.4.17"
+    semver = "2.4.18"
     wrapper = False
     wrapped_payloads = ["scarecrow_wrapper", "service_wrapper"]
     c2_profiles = ["http", "httpx", "smb", "tcp", "websocket", "azure_blob"]
