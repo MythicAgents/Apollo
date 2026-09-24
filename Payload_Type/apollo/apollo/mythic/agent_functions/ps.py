7fdc6e7dbce2cd497eacabce822fd22bbf536cf5
Jack Ullrich <mail@jackullri.ch>
2026-09-24 13:48:37 -0400

    Refactor ps with limited process access

--- Payload_Type/apollo/apollo/mythic/agent_functions/ps.py
+++ Payload_Type/apollo/apollo/mythic/agent_functions/ps.py
@@ -6,20 +6,34 @@ class PsArguments(TaskArguments):
 
     def __init__(self, command_line, **kwargs):
         super().__init__(command_line, **kwargs)
-        self.args = []
+        self.args = [
+            CommandParameter(
+                name="extended",
+                type=ParameterType.Boolean,
+                default_value=False,
+                description="Include parent PID, command line, window title, and executable file metadata.",
+                parameter_group_info=[
+                    ParameterGroupInfo(required=False, ui_position=1, group_name="Default")
+                ],
+            )
+        ]
 
     async def parse_arguments(self):
-        if len(self.command_line.strip()) > 0:
-            raise Exception("ps takes no command line arguments.")
-        pass
+        command = self.command_line.strip()
+        if command.startswith("{"):
+            self.load_args_from_json_string(command)
+        elif command == "--extended":
+            self.add_arg("extended", True, ParameterType.Boolean)
+        elif command:
+            raise Exception("Usage: ps [--extended]")
 
 
 class PsCommand(CommandBase):
     cmd = "ps"
     needs_admin = False
-    help_cmd = "ps"
-    description = "Get a brief process listing with basic information."
-    version = 3
+    help_cmd = "ps [--extended]"
+    description = "List processes with limited query access; --extended adds slower process details."
+    version = 4
     supported_ui_features = ["process_browser:list"]
     author = "@djhohnstein"
     argument_class = PsArguments
