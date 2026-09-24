9fe34b37b0b945400c73f27b3c20b152ca390c23
Jack Ullrich <mail@jackullri.ch>
2026-09-24 14:39:52 -0400

    Hide unknown ps IDs in browser table

--- Payload_Type/apollo/apollo/agent_code/Tasks/ps.cs
+++ Payload_Type/apollo/apollo/agent_code/Tasks/ps.cs
@@ -174,10 +174,8 @@ namespace Tasks
             Process[] processes;
             try
             {
-                // On local Windows, .NET Framework queries system-wide process
-                // information and constructs Process objects from the result;
-                // it does not open a handle to each process here. Process.Handle
-                // would request all access, so avoid it.
+                // .NET Framework takes a system process snapshot here; it does not
+                // open handles. Avoid Process.Handle, which opens with all access.
                 processes = Process.GetProcesses();
             }
             catch (Exception ex)
